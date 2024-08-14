edd2955fcf572064d9069613abd40017673f2708
Geo Bursuc <g.bursuc@software-dep.net>
2024-08-14 10:37:39 +0300

    Enabled push notifications for iOS

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by bss on 14.08.2024.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
