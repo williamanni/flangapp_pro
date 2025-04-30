5f05d4a2db8c1a71aee8fcc2b50166faa94f997f
Geo Bursuc <g.bursuc@software-dep.net>
2025-04-30 10:10:17 +0300

    Prepared app for push notifications

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by bss on 30.04.2025.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
